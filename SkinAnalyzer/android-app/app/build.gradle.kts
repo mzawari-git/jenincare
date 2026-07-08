ba8bfb2d6cb306316789d12450f283fa8f56b1de
DermaPulse <info@dermapulseco.shop>
2026-07-08 17:33:11 +0300

    v1.2.46 version bump

--- SkinAnalyzer/android-app/app/build.gradle.kts
+++ SkinAnalyzer/android-app/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 24
         //noinspection EditedTargetSdkVersion
         targetSdk = 33
-        versionCode = 45
-        versionName = "1.2.45"
+        versionCode = 46
+        versionName = "1.2.46"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
